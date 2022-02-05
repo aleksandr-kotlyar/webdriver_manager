a35def16f7a38eee8278971699d38cf0b64b795f
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2022-02-05 17:50:56 +0300

    [gh #261] determine browser-versions on Windows
    Also:
    - stabilize tests;
    - update changelog with 3.5.3;
    - preprare to 3.5.3;

--- tests/test_edge_driver.py
+++ tests/test_edge_driver.py
@@ -15,19 +15,15 @@ def test_edge_manager_with_selenium():
     driver.quit()
 
 
-def test_driver_with_ssl_verify_disabled_can_be_downloaded():
-    try:
-        os.environ['WDM_SSL_VERIFY'] = '0'
-        custom_path = os.path.join(
-            os.path.dirname(os.path.dirname(__file__)),
-            "ssl_disabled",
-        )
-        driver_path = EdgeChromiumDriverManager(path=custom_path).install()
+def test_driver_with_ssl_verify_disabled_can_be_downloaded(ssl_verify_enable):
+    os.environ['WDM_SSL_VERIFY'] = '0'
+    custom_path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "ssl_disabled",
+    )
+    driver_path = EdgeChromiumDriverManager(path=custom_path).install()
 
-        assert os.path.exists(driver_path)
-
-    finally:
-        os.environ['WDM_SSL_VERIFY'] = ''
+    assert os.path.exists(driver_path)
 
 
 def test_edge_manager_with_wrong_version():
@@ -36,26 +32,11 @@ def test_edge_manager_with_wrong_version():
             version="0.2",
             os_type='win64',
         ).install()
-        driver = webdriver.Edge(executable_path=driver_path)
-        driver.quit()
-
+        webdriver.Edge(executable_path=driver_path)
     assert (
                "There is no such driver by url "
                "https://msedgedriver.azureedge.net/0.2/edgedriver_win64.zip"
            ) in ex.value.args[0]
-
-
-@pytest.mark.parametrize('os_type', ['win32', 'win64', 'linux64', 'mac64'])
-def test_can_download_edge_driver(os_type):
-    path = EdgeChromiumDriverManager(os_type=os_type).install()
-    assert os.path.exists(path)
-
-
-@pytest.mark.parametrize('os_type', ['win32', 'win64', 'mac64', 'linux64'])
-def test_can_get_edge_driver_from_cache(os_type):
-    EdgeChromiumDriverManager(os_type=os_type).install()
-    driver_path = EdgeChromiumDriverManager(os_type=os_type).install()
-    assert os.path.exists(driver_path)
 
 
 @pytest.mark.parametrize('os_type', ['win32', 'win64', 'mac64', 'linux64'])
@@ -66,3 +47,17 @@ def test_edge_with_specific_version(os_type, specific_version):
         os_type=os_type,
     ).install()
     assert os.path.exists(bin_path)
+
+
+@pytest.mark.parametrize('os_type', ['win32', 'win64', 'mac64', 'linux64'])
+@pytest.mark.parametrize('specific_version', ['87.0.637.0'])
+def test_can_get_edge_driver_from_cache(os_type, specific_version):
+    EdgeChromiumDriverManager(
+        version=specific_version,
+        os_type=os_type,
+    ).install()
+    driver_path = EdgeChromiumDriverManager(
+        version=specific_version,
+        os_type=os_type
+    ).install()
+    assert os.path.exists(driver_path)
